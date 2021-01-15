db38d786a6dc2168498ad90067f145836b095687
Your Name <prbizarro@gmail.com>
2021-01-15 12:08:02 -0300

    Now we can catch product price and name for the specified URL and list it on the screen

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
+import string
 
-html_text = requests.get(
-    'https://www.boadica.com.br/pesquisa/multi_playstation/precos?ClasseProdutoX=2&CodCategoriaX=13&XF=48&XT=8&XE=1&modelo=179320&regiao=&em_box=&cl=&preco_min=&preco_max=').text
+# Pegando lista de produtos
+html_text = requests.get('https://www.boadica.com.br/pesquisa/multi_playstation/precos?ClasseProdutoX=2&CodCategoriaX=13&XT=8&XE=1&XF=48').text
 soup = BeautifulSoup(html_text, 'lxml')
-precos = soup.find_all('div', class_='row preco detalhe')
-print(precos)
+produtos = soup.find_all('div', class_ = 'row preco detalhe')
+
+for produto in produtos:
+    # Pegando preco do produto
+    preco_produto = produto.find('div', class_='col-md-1 preco').text.replace('\n', '').replace(' ', '')
+
+    #Pegando o modelo do produto
+    coluna_modelo = produto.find('div', class_='col-md-2 center')
+    modelo_produto = coluna_modelo.find('div', class_='no-mobile').text.split()
+    separator = ''
+    modelo_produto =separator.join(modelo_produto)
+        
+
+    print("Preço do produto:")
+    print(preco_produto)
+    print("Modelo do produto:")
+    print(modelo_produto)
+    print('')
